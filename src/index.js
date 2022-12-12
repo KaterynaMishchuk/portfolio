a78c9afabf960c1decc92be036ba599d75d44cb1
KaterynaMishchuk <8343636@gmail.com>
2022-12-13 01:17:13 +0200

    Scroll To Top Button Added

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let upbutton = document.getElementById("upBtn");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    upbutton.style.display = "block";
+  } else {
+    upbutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
